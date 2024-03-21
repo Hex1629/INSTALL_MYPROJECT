6ad582a9b214cbf10bfd525dcaf23785ce3a3170
HaxTermux <93824226+Hex1629@users.noreply.github.com>
2024-03-21 10:04:36 +0700

    Update vps.sh

--- INSTALL_SCRIPT/vps.sh
+++ INSTALL_SCRIPT/vps.sh
@@ -1,1 +1,11 @@
-apt install python-is-python3
+
+#!/bin/bash
+
+# Check if the system has a hypervisor
+if [ -d "/proc/sys/xen" ] || [ -d "/proc/sys/vmware" ] || [ -d "/proc/sys/qemu" ]; then
+    echo "Running on a VPS."
+    apt install python-is-python3
+else
+    echo "Not running on a VPS. Exiting..."
+    exit 1
+fi
